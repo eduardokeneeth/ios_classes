9d4f701522532933e9b2c81d92dfbb74bb09629d
Usuário Convidado <Guest@L2602MICRO47.local>
2016-08-08 21:46:29 -0300

    Init Repo

--- Exemplo1_TableView_Swift/ViewController.swift
+++ Exemplo1_TableView_Swift/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var minhaTabView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        <#code#>
     }
 
 
