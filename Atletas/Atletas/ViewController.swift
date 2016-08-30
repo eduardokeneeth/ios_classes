092665e7cf41329e5bac2c1a50025593648bb006
Usuário Convidado <Guest@L2602MICRO12.local>
2016-08-29 22:31:42 -0300

    Showing athletes, countries and images.

--- Atletas/Atletas/ViewController.swift
+++ Atletas/Atletas/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var meuLabel: UILabel!
+    var textoDoLabel:String = ""
+    
+    @IBOutlet weak var countryLabel: UILabel!
+    var textoDoCountryLabel:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        meuLabel.text = textoDoLabel
+        countryLabel.text = textoDoCountryLabel
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
